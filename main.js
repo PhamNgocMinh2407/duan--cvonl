0e244d7fc349fdf24872e609e9bf4052a2688914
PhamNgocMinh2407 <vopham689@gmail.com>
2026-05-23 11:51:46 +0700

    done

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+// ===== MENU MOBILE =====
 
 const menuBtn =
 document.querySelector(".menu-btn");
 
 const navLinks =
 document.querySelector(".nav-links");
+
 if(menuBtn && navLinks){
 
     menuBtn.addEventListener("click", () => {
@@ -13,6 +15,8 @@ if(menuBtn && navLinks){
     });
 
 }
+
+// ===== AVATAR UPLOAD + SAVE =====
 
 const fileInput =
 document.getElementById("fileInput");
@@ -25,25 +29,70 @@ document.getElementById("avatarWrapper");
 
 if(avatarWrapper && fileInput && preview){
 
+    // ===== LOAD AVATAR ĐÃ LƯU =====
+
+    const savedAvatar =
+    localStorage.getItem("avatar");
+
+    if(savedAvatar){
+
+        preview.src = savedAvatar;
+
+        preview.style.display =
+        "block";
+
+        avatarWrapper.classList.add(
+            "has-image"
+        );
+
+    }
+
+    // ===== CLICK AVATAR =====
+
     avatarWrapper.addEventListener("click", () => {
 
         fileInput.click();
 
     });
 
-   fileInput.addEventListener("change", function(){
+    // ===== CHỌN ẢNH =====
+
+    fileInput.addEventListener("change", function(){
 
         const file = this.files[0];
 
         if(file){
 
-            const imageURL =
-            URL.createObjectURL(file);
-            preview.src = imageURL;
-           preview.style.display = "block";
-            avatarWrapper.classList.add(
-                "has-image"
-            );
+            const reader =
+            new FileReader();
+
+            reader.onload = function(e){
+
+                const imageData =
+                e.target.result;
+
+                // cập nhật ảnh
+
+                preview.src =
+                imageData;
+
+                preview.style.display =
+                "block";
+
+                avatarWrapper.classList.add(
+                    "has-image"
+                );
+
+                // lưu ảnh
+
+                localStorage.setItem(
+                    "avatar",
+                    imageData
+                );
+
+            };
+
+            reader.readAsDataURL(file);
 
         }
 
